b9e2b49237b2b38285130a57573922af089ef0b5
sbaumgertner <s.v.baumgertner@gmail.com>
2022-08-28 20:20:52 +0400

    Revert "feat: implement codejam-eldritch"
    This reverts commit 841083e5971657fa43869af2d55165fac5d05fda.

--- data/mythicCards/index.js
+++
@@ -1,9 +0,0 @@
-import {cardsData as brownCards} from './brown/index.js';
-import {cardsData as blueCards} from './blue/index.js';
-import {cardsData as greenCards} from './green/index.js';
-
-export {
-  brownCards,
-  blueCards,
-  greenCards
-}
